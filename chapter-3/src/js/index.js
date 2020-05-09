e18fb9e5be462026890e8c9ec753b7f84d0f07dd
Takumi0901 <juventus0901@gmail.com>
2020-05-09 16:56:01 +0900

    chapter 3

--- chapter-3/src/js/index.js
+++ chapter-3/src/js/index.js
@@ -1,5 +1,21 @@
-const app = document.getElementById("app");
-const p = document.createElement("p");
-p.innerText = "Hello World!";
+import h from './createElement'
+import { render, mount } from './render'
 
-app.appendChild(p);
+const view = h('div', {
+  attrs: {
+    id: 'app'
+  },
+  children: [
+    h('p', {
+      attrs: {},
+      children: ['仮想DOMの学習スタート！']
+    })
+  ]
+})
+
+const $app = render(view)
+
+console.log('*****************')
+console.log($app)
+console.log('*****************')
+mount($app, document.getElementById('app'))

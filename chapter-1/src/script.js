bc757e3853b93c4c14e57cbe5d1fa3eec43e5322
Takumi0901 <juventus0901@gmail.com>
2020-05-20 12:15:04 +0900

    fix jQuery

--- chapter-1/src/script.js
+++ chapter-1/src/script.js
@@ -1,4 +1,12 @@
 $(function () {
+  $(".js-account-item").each(function () {
+    var $this = $(this);
+    var $thisData = $this.data();
+    if ($thisData.follow) {
+      $this.find(".js-follow-btn").addClass("isFollow");
+    }
+  });
+
   $(".js-follow-btn").on("click", function () {
     var $this = $(this);
     var $accountItem = $this.parents(".js-account-item");

6f4f9e720c3ad8a2f482396e84c5b175de6165c3
beginerbeginer <miyabi6125@gmail.com>
2022-01-03 21:27:50 +0900

    add: comment

--- chapter-5/src/js/vdom/app.js
+++ chapter-5/src/js/vdom/app.js
@@ -1,9 +1,15 @@
 import { render } from './render'
 
+// 変数appを定義しroot,initialState,viewを受け取るようにする
 export const app = ({ root, initialState, view }) => {
+
+  // appendChildで追加したいリアルDOMを取得する
   const $el = document.querySelector(root)
+
+  // リアルDOMに反映する仮想DOM用の変数newNodeを定義
   let newNode = view(initialState)
 
+  // newNodeを使って作成したリアルDOMをrootに反映するrenderDOMメソッドを実行
   const renderDOM = function () {
     $el.appendChild(render(newNode))
   }
--- chapter-5/src/js/vdom/render.js
+++ chapter-5/src/js/vdom/render.js
@@ -9,7 +9,7 @@ const setAttrs = (target, attrs) => {
     }
   }
 }
-
+// 渡された要素のtagNameを元にリアルDOM要素を作成 (createElement) する。
 function renderElement({ tagName, attrs, children }) {
   const $el = document.createElement(tagName)
 
@@ -22,9 +22,13 @@ function renderElement({ tagName, attrs, children }) {
   return $el
 }
 
+// 渡された要素が文字列か数字かを判定する
 export function render(vNode) {
+  // 文字列または数字の場合はそのまま返す
   if (typeof vNode === 'string') {
+    // 文字列をDOMに変換して返す
     return document.createTextNode(vNode)
   }
+  // 文字列でない場合 (オブジェクトの場合)renderElement関数が呼ばれる
   return renderElement(vNode)
 }

f583cc419445a5106552bdb31ee049dd586e8ef1
beginerbeginer <miyabi6125@gmail.com>
2022-01-03 11:30:20 +0900

    add: comment

--- chapter-3/src/js/vdom/createElement.js
+++ chapter-3/src/js/vdom/createElement.js
@@ -1,6 +1,7 @@
 export default (tagName, { attrs = {}, children = [] }) => {
   const vElement = Object.create(null)
-
+  // h()に渡された引数をtagName, attrs, children を持っている JSのオブジェクトに変換
+  //vElementとは? →仮想DOMの要素を表すオブジェクト
   Object.assign(vElement, {
     tagName,
     attrs,

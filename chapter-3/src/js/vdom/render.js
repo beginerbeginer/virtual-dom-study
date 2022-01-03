36715243afd7b8d486a9ee2134fe565dcf2360de
beginerbeginer <miyabi6125@gmail.com>
2022-01-03 15:24:47 +0900

    add: comment

--- chapter-3/src/js/vdom/render.js
+++ chapter-3/src/js/vdom/render.js
@@ -1,20 +1,26 @@
+// 渡された要素のtagNameを元にリアルDOM要素を作成 (createElement) する。
 function renderElement({ tagName, attrs, children }) {
+  // 仮想DOMをリアルDOMに変換する
   const $el = document.createElement(tagName)
-
+  // 仮想DOMに属性を追加する
   for (const [k, v] of Object.entries(attrs)) {
     $el.setAttribute(k, v)
   }
-
+  // 仮想DOMに子要素を追加する(childrenは配列)
   for (const child of children) {
     $el.appendChild(render(child))
   }
-
+  // リアルDOMを返す
   return $el
 }
 
+// 渡された要素が文字列かどうかを判定する
 export function render(vNode) {
+  // 文字列の場合
   if (typeof vNode === 'string') {
+    // 文字列をDOMに変換して返す
     return document.createTextNode(vNode)
   }
+  // 文字列でない場合 (オブジェクトの場合)renderElement関数が呼ばれる
   return renderElement(vNode)
 }
